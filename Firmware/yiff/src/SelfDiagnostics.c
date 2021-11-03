a691b00c813fb94a532ea9c9438bc9a821cba6cf
White Fossa <whitefossa@gmail.com>
2021-11-03 19:56:58 +0300

    Fixed a bug in self-diagnostics

--- Firmware/yiff/src/SelfDiagnostics.c
+++ Firmware/yiff/src/SelfDiagnostics.c
@@ -15,11 +15,22 @@ void SelfDiagnostics_HaltOnFailure(YhlFailureCausesEnum failureCause)
 	HAL_EmergencyShutdown();
 
 	/* Logging error (if EEPROM is accessable) */
-	if (EEPROMContext.IsFound && !IsEEPROMHeadersInitialized)
+	if (EEPROMContext.IsFound && IsEEPROMHeadersInitialized)
 	{
 		EEPROM_Header.LastFailure = failureCause;
 		EEPROM_WriteHeader(&EEPROM_Header, EEPROM_ConstantHeader.HeaderAddress);
 	}
+
+	L2HAL_ERROR_CLOCK_SIGNAL_PORT();
+
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+	GPIO_InitStruct.Pin = L2HAL_ERROR_SIGNAL_PIN;
+	HAL_GPIO_Init(L2HAL_ERROR_SIGNAL_PORT, &GPIO_InitStruct);
 
 	/* Halt */
 	L2HAL_Error(Generic);
